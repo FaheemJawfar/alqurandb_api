efeae25efb91cdd17aec8d0bd335342b6e851684
Faheem <faheemjawfar@gmail.com>
2023-12-21 14:46:41 +0530

    get endpoint updated

--- routes/quranTranslationsRoutes.js
+++ routes/quranTranslationsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const quranTranslationsController = require('../controllers/quranTranslationsController');
+//const quranTranslationsController = require('../controllers/quranTranslationsController');
 const fileUtils = require('../utils/fileUtils');
 const path = require('path');
 const router = express.Router();
@@ -9,5 +9,23 @@ router.get('/xml/:filename', (req, res) => {
   const filePath = path.join(__dirname, '..', 'quran_translations/xml', filename);
   fileUtils.checkAndSendFile(res, filePath, filename);
 });
+
+
+router.get('/', (req, res) => {
+  // Extract translation and fileformat from query parameters
+  const translation = req.query.translation;
+  const fileformat = req.query.fileformat;
+
+  // Construct filename using translation and fileformat
+  const filename = `${translation}.${fileformat}`;
+
+  // Construct the file path
+  const filePath = path.join(__dirname, '..', 'quran_translations/xml', filename);
+
+  // Send the file
+  fileUtils.checkAndSendFile(res, filePath, filename);
+});
+
+
 
 module.exports = router;
