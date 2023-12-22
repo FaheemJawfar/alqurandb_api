22476a9a3b4917f4d8d5955804b759147ed15ca8
Faheem <faheemjawfar@gmail.com>
2023-12-22 14:41:32 +0530

    getAyaTranslation endpoint

--- routes/quranTranslationsRoutes.js
+++ routes/quranTranslationsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-//const quranTranslationsController = require('../controllers/quranTranslationsController');
-const fileUtils = require('../utils/fileUtils');
+const xml2js = require('xml2js');
+const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
@@ -25,6 +25,44 @@ router.get('/', (req, res) => {
   // Send the file
   fileUtils.checkAndSendFile(res, filePath, filename);
 });
+
+
+
+
+router.get('/getAyaTranslation', (req, res) => {
+  const suraIndex = req.query.sura;
+  const ayaIndex = req.query.aya;
+  const filename = req.query.translation + '.xml';
+
+  const filePath = path.join(__dirname, '..', 'quran_translations/xml', filename);
+
+  fs.readFile(filePath, (err, data) => {
+      if (err) {
+          res.status(500).send('Error reading the XML file');
+          return;
+      }
+
+      xml2js.parseString(data, (err, result) => {
+          if (err) {
+              res.status(500).send('Error parsing the XML file');
+              return;
+          }
+
+          try {
+              const ayaText = result.quran.sura.find(s => s.$.index === suraIndex).aya.find(a => a.$.index === ayaIndex).$.text;
+              // Returning the result in JSON format
+              res.json({
+                  sura: suraIndex,
+                  aya: ayaIndex,
+                  text: ayaText
+              });
+          } catch (error) {
+              res.status(404).send('Sura or Aya not found');
+          }
+      });
+  });
+});
+
 
 
 
