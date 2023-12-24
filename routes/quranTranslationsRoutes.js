e724da2b1f69aa61ef45d4e30e6f744ddab9a06c
Faheem <faheemjawfar@gmail.com>
2023-12-24 12:29:14 +0530

    added sqlitedb

--- routes/quranTranslationsRoutes.js
+++ routes/quranTranslationsRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const xml2js = require('xml2js');
 const fs = require('fs');
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 const router = express.Router();
 
 router.get('/xml/:filename', (req, res) => {
@@ -29,7 +30,7 @@ router.get('/', (req, res) => {
 
 
 
-router.get('/getAyaTranslation', (req, res) => {
+router.get('/getAyaTranslationXML', (req, res) => {
   const suraIndex = req.query.sura;
   const ayaIndex = req.query.aya;
   const filename = req.query.translation + '.xml';
@@ -61,6 +62,55 @@ router.get('/getAyaTranslation', (req, res) => {
           }
       });
   });
+});
+
+
+
+
+router.get('/getAyaTranslation', (req, res) => {
+    const suraIndex = req.query.sura;
+    const ayaIndex = req.query.aya;
+    const filename = req.query.translation + '.db';
+
+    const dbPath = path.join(__dirname, '..', 'quran_translations/sqlite', filename);
+
+    // Open the database
+    let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Error connecting to the database');
+            return;
+        }
+        console.log('Connected to the SQLite database.');
+    });
+
+    const query = `SELECT text FROM tamil_translation WHERE sura_number = ? AND aya_number = ?`;
+    
+    // Execute the query
+    db.get(query, [suraIndex, ayaIndex], (err, row) => {
+        if (err) {
+            res.status(500).send('Error executing the query');
+            return;
+        }
+
+        if (row) {
+            res.json({
+                sura: suraIndex,
+                aya: ayaIndex,
+                text: row.text
+            });
+        } else {
+            res.status(404).send('Sura or Aya not found');
+        }
+    });
+
+    // Close the database connection
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Closed the database connection.');
+    });
 });
 
 
