6913b92d9d0c4983103c85441778bf9eda46d0b6
Faheem <faheemjawfar@gmail.com>
2023-12-24 18:38:02 +0530

    middleware update

--- middleware/loggingMiddleware.js
+++ middleware/loggingMiddleware.js
@@ -1,5 +1,10 @@
 const morgan = require('morgan');
 
-const loggingMiddleware = morgan(':remote-addr :remote-user :method :url :status :res[content-length] - :response-time ms');
+morgan.token('client-ip', (req) => {
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  return xForwardedFor ? xForwardedFor.split(',')[0] : req.socket.remoteAddress;
+});
+
+const loggingMiddleware = morgan(':client-ip :remote-user :method :url :status :res[content-length] - :response-time ms');
 
 module.exports = loggingMiddleware;
